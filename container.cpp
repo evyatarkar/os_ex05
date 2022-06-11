beaea44c4ce9707405c47bf0a19f1da2d12cb682
evyatarkar <80752596+evyatarkar@users.noreply.github.com>
2022-06-11 19:24:20 +0300

    end day 1!!! saves files and directories

--- container.cpp
+++ container.cpp
@@ -18,16 +18,17 @@
 #include <iostream>
 #include <fstream>
 #include <csignal>
+#include <cstdlib>
 
 #define STACK 8192
 #define MKDIR_MODE 0755
 
 
-void print_cur_dir(){
-  char tmp[256];
-  getcwd(tmp, 256);
-  std::cout << "Get: " << tmp << std::endl;
-}
+//void print_cur_dir(){
+//  char tmp[256];
+//  getcwd(tmp, 256);
+//  std::cout << "Get: " << tmp << std::endl;
+//}
 
 void make_directory (char *name)
 {
@@ -54,46 +55,15 @@ void create_cgroup_directories_pids ()
   chdir ("pids");
 }
 
-//void write_string_to_file (char *path, char *filename, char *content)
-//{
-//  chdir (path);
-//  if (access (filename, MKDIR_MODE) == 0)
-//    {
-//      std::cerr
-//          << "system error: failed finding file: " <<
-//          path << "/" << filename
-//          << std::endl;
-//      exit (1);
-//    }
-//  std::ofstream File (filename);
-//  File << content << std::endl;
-//  File.close ();
-//}
-
-void write_int_to_file (char *path, char *filename, int content)
-{
-  chdir (path);
-  std::ofstream file;
-  file.open (filename);
-  std::cout << "file: " << filename << " created." << std::endl;
-
-  file << content << "\n";
-  std::cout << "-- writing in file: " << content << std::endl;
-  file.close ();
-  std::cout << "-- done writing" << std::endl;
-}
 
 void write_int_to_file (char *path, int content)
 {
   std::ofstream file_to_write_to(path);
-//  file_to_write_to.open (path);
   std::cout << "file_to_write_to: " << path << " created." << std::endl;
-
   file_to_write_to << content;
   std::cout << "-- writing in file_to_write_to: " << content << std::endl;
   file_to_write_to.close ();
   std::cout << "-- done writing" << std::endl;
-  print_cur_dir();
 }
 
 int child (void *args)
@@ -101,17 +71,16 @@ int child (void *args)
 
   char **temp = (char**) args;
   std::cout << "running child function" << std::endl;
-  for (int  i = 0; i < 5; i++){
-      std::cout << "args[" << i << "] = " << *(temp + i) << std::endl;
-
-    }
+//  for (int  i = 0; i < 5; i++){
+//      std::cout << "args[" << i << "] = " << *(temp + i) << std::endl;
+//    }
 
   // change root folder
   char *path = *(temp + 2);
   std::cout << "got path: " << path << std::endl;
   chroot (path);
   chdir (path);
-  print_cur_dir();
+
   // cgroups initialization of directories
   create_cgroup_directories_pids ();
 
@@ -120,24 +89,18 @@ int child (void *args)
   std::cout << "got hostname: " << name << std::endl;
   sethostname (name, strlen (name));
 
-//  chdir ((char *) "/sys/fs/cgroup/pids");
   write_int_to_file ((char *) "/sys/fs/cgroup/pids/cgroup.procs",
                      getpid ());
- int * a = (int *) *(temp + 3);
-//  chdir ((char *) "/sys/fs/cgroup/pids");
   write_int_to_file ((char *) "/sys/fs/cgroup/pids/pids.max",
-                     *a);
+                     atoi(*(temp + 3)));
 
   // mount new procfs
   chdir (path);
-  std::cout << "changed dir to: " << path << std::endl;
+//  std::cout << "changed dir to: " << path << std::endl;
 
-  print_cur_dir();
   mount ("proc", "/proc", "proc", 0, 0);
 
-
   // run terminal/new program
-
   char *_args[] = {*(temp + 4), *(temp + 5), (char *) 0};
   int ret = execvp (*(temp + 4), _args);
 
@@ -191,10 +154,10 @@ void signal_handler (char *path)
 
 int main (int argc, char *argv[])
 {
-  for (int  i = 0; i < argc; i++){
-      std::cout << "args[" << i << "] = " << argv[i] << std::endl;
-
-    }
+//  for (int  i = 0; i < argc; i++){
+//      std::cout << "args[" << i << "] = " << argv[i] << std::endl;
+//
+//    }
 //  signal(SIGCHLD, signal_handler);
   // create new process
 //  chroot ((char *) argv + 2);
